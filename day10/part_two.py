91ec2af8aeab8bb7c434c2b2ea90f354d5c810cd
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2025-12-11 00:12:12 +0200

    something working

--- day10/part_two.py
+++ day10/part_two.py
@@ -1,9 +1,12 @@
 import math
+import multiprocessing
 import random
+import time
 
 from shared import PuzzleInput, load
 
-FIND_MIN_STEPS: int = 100
+WORKERS: int = 14
+FIND_TIME_WINDOW: float = 15.0
 
 MAX_STEPS: int = int(1e9)
 
@@ -11,7 +14,7 @@ TEMP_START: float = 100.0
 TEMP_END: float = 1e-9
 COEF_STEP: int = 100
 PLACES_TO_SWAP: int = 10
-COOLING_FACTOR: float = 0.995
+COOLING_FACTOR: float = 0.9999
 
 
 def cast_buttons_to_list(buttons: list[int], lights_count: int) -> list[list[int]]:
@@ -56,6 +59,49 @@ def get_neighbor(state, T, max_joltage):
         new_state[i] = max(0, new_state[i])
         new_state[i] = min(max_joltage, new_state[i])
     return new_state
+
+
+def sa_worker(puzzle, timeout):
+    start_time = time.time()
+
+    best_state, best_energy = None, float('inf')
+    max_joltage = max(puzzle.joltage)
+
+    buttons = cast_buttons_to_list(puzzle.light_buttons, len(puzzle.joltage))
+
+    while time.time() - start_time < timeout:
+        state = [random.randint(0, max(puzzle.joltage)) for _ in range(len(puzzle.light_buttons))]
+        energy = get_energy(state, buttons, puzzle.joltage)
+
+        T = TEMP_START
+        while T > TEMP_END and time.time() - start_time < timeout:
+            new_state = get_neighbor(state, T, max_joltage)
+            new_energy = get_energy(new_state, buttons, puzzle.joltage)
+            dt_energy = new_energy - energy
+            if dt_energy < 0 or random.random() < math.exp(-dt_energy / T):
+                state = new_state
+                energy = new_energy
+                if energy < best_energy:
+                    current: list[int] = calc(state, buttons, len(puzzle.joltage))
+                    if current == puzzle.joltage:
+                        best_state = state
+                        best_energy = energy
+            T *= COOLING_FACTOR
+
+    return best_state, best_energy
+
+
+def solve_parallel(puzzle):
+    with multiprocessing.Pool(WORKERS) as pool:
+        results = [pool.apply_async(sa_worker, (puzzle, FIND_TIME_WINDOW)) for _ in range(WORKERS)]
+        output = [r.get() for r in results]
+
+    best_state, best_energy = min(output, key=lambda x: x[1])
+    if best_state is None:
+        return -1
+
+    print(f'[!] {puzzle.joltage} -> best minimal: {sum(best_state)} (energy: {best_energy})')
+    return sum(best_state)
 
 
 def solve(puzzle: PuzzleInput) -> int:
@@ -130,12 +176,24 @@ def solve_puzzle(puzzle_data: list[PuzzleInput]) -> int:
     return total
 
 
+def solve_puzzlev2(puzzle_data: list[PuzzleInput]) -> int:
+    total: int = 0
+    for puzzle in puzzle_data:
+        result = solve_parallel(puzzle)
+        while result == -1:
+            print(f'Rerun: {puzzle.joltage}')
+            result = solve_parallel(puzzle)
+        total += result
+    return total
+
+
 if __name__ == '__main__':
     # filename: str = 'example.txt'
     filename: str = 'puzzle.txt'
 
     puzzle_data: list[PuzzleInput] = load(filename)
 
-    part_two: int = solve_puzzle(puzzle_data)
+    # part_two: int = solve_puzzle(puzzle_data)
+    part_two: int = solve_puzzlev2(puzzle_data)
     print(f'Part II: {part_two}')
 
